d342a1c12032f0fdfbf653e0186401de9381ef7b
Aleksey <astynax84@gmail.com>
2015-08-30 22:20:08 +0300

    some refactoring and imports prettifying

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Network.Wai
-import Network.HTTP.Types (status200, hContentType)
-import Network.Wai.Handler.Warp (run)
+import           Data.ByteString.Lazy     (ByteString)
+import           Network.HTTP.Types       (Status, hContentType, notFound404,
+                                           status200)
+import           Network.Wai              (Application, Response, responseLBS)
+import           Network.Wai.Handler.Warp (run)
 
 application :: Application
-application _ respond = respond $
-  responseLBS status200 [(hContentType, "text/plain")] "Hello World"
+application _ respond = respond $ responseOk "Hello World"
 
 main :: IO ()
 main = do
   putStrLn "Serving (hit Ctrl+C to stop)..."
   run 8000 application
+
+
+responseOk :: ByteString -> Response
+responseOk = responsePlainText status200
+
+response404 :: ByteString -> Response
+response404 = responsePlainText notFound404
+
+responsePlainText :: Status -> ByteString -> Response
+responsePlainText = (`responseLBS` [(hContentType, "text/plain")])
+
