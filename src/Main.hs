d78d96770adcd88d2ec2182fc18f83cdf638abed
Aleksey <astynax84@gmail.com>
2015-09-01 15:10:09 +0300

    All functionality implemented

--- src/Main.hs
+++ src/Main.hs
@@ -2,26 +2,108 @@
 module Main where
 
 import           Data.ByteString.Lazy     (ByteString)
-import           Network.HTTP.Types       (Status, hContentType, notFound404,
-                                           status200)
-import           Network.Wai              (Application, Response, responseLBS)
+import qualified Data.ByteString.Lazy     as LBS
+import qualified Data.ByteString.Char8    as BS
+import           Data.Char                (toUpper)
+import           Data.Map.Strict          (Map, fromList, keys, lookup)
+import           Network.HTTP.Types       (Status, badRequest400, hContentType,
+                                           methodGet, notFound404, status200,
+                                           statusCode)
+import           Network.Wai              (Application, Response, Middleware,
+                                           rawPathInfo, rawQueryString,
+                                           requestMethod, responseLBS,
+                                           queryString, rawPathInfo,
+                                           responseStatus)
 import           Network.Wai.Handler.Warp (run)
+import           Prelude                  hiding (lookup)
+
+-- Library of functions
+
+type FunctionName        = BS.ByteString
+type FunctionDescription = BS.ByteString
+type FunctionArg         = BS.ByteString
+type FunctionResult      = BS.ByteString
+type FunctionSpec        = ( FunctionDescription
+                           , (FunctionArg -> FunctionResult) )
+
+library :: Map FunctionName FunctionSpec
+library =
+  fromList [ ("reverse", ( "returns string with characters in reverset order",
+                           BS.reverse ))
+           , ("upper",   ( "returns string with each character in upper case",
+                           BS.map toUpper ))]
+
+getFunctionSpec :: FunctionName -> Maybe FunctionSpec
+getFunctionSpec = (`lookup` library)
+
+listOfFunctions :: [FunctionName]
+listOfFunctions = keys library
+
+describe :: FunctionSpec -> FunctionDescription
+describe = fst
+
+call :: FunctionSpec -> FunctionArg -> FunctionResult
+call = snd
+
+-- Web Application
 
 application :: Application
-application _ respond = respond $ responseOk "Hello World"
+application req respond
+  | requestMethod req /= methodGet =
+    respond
+    $ responseBadRequest "Only GET method is allowed!"
+
+  | path == "" =
+    respond
+    $ if query /= ""
+      then responseBadRequest "No query parameters needed!"
+      else responseOk renderedListOfFunctions
+
+  | otherwise =
+    respond
+    $ maybe
+    (responseNotFound "Unknown function!")
+    (\spec -> responseOk
+              $ LBS.fromStrict
+              $ if query == ""
+                then describe spec
+                else call spec firstArg)
+    $ getFunctionSpec path
+
+  where
+    query = rawQueryString req
+    path  = BS.tail $ rawPathInfo req
+
+    firstArg = fst $ head $ queryString req
+
+    renderedListOfFunctions =
+      LBS.intercalate "\n"
+      $ "Available functions:" : map LBS.fromStrict listOfFunctions
+
+-- Main function
 
 main :: IO ()
 main = do
   putStrLn "Serving (hit Ctrl+C to stop)..."
-  run 8000 application
+  run 8000 $ withLogging application
 
+-- Misc functions
 
-responseOk :: ByteString -> Response
-responseOk = responsePlainText status200
-
-response404 :: ByteString -> Response
-response404 = responsePlainText notFound404
+responseOk, responseNotFound, responseBadRequest :: ByteString -> Response
+responseOk         = responsePlainText status200
+responseNotFound   = responsePlainText notFound404
+responseBadRequest = responsePlainText badRequest400
 
 responsePlainText :: Status -> ByteString -> Response
 responsePlainText = (`responseLBS` [(hContentType, "text/plain")])
 
+-- Logging
+
+withLogging :: Middleware
+withLogging app req respond =
+  app req $ \response -> do
+    putStrLn $ statusOf response ++ ": " ++ query
+    respond response
+  where
+    query = BS.unpack $ BS.concat [rawPathInfo req, rawQueryString req]
+    statusOf = show . statusCode . responseStatus
